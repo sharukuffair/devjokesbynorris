6784aef45885c121eca111c4e45d0f11f958490f
sharuk uffair <sharukuffair1998@gmail.com>
2023-10-03 21:18:09 +0530

    init added

---
+++ app.js
@@ -0,0 +1,30 @@
+const jokeContainer = document.querySelector(".content");
+const btnNext = document.querySelector(".next");
+
+// default case
+jokeContainer.textContent = fetchJokesData();
+
+// API CALL
+async function fetchJokesData() {
+  try {
+    let jokes = await fetch(
+      "https://api.chucknorris.io/jokes/random?category=dev"
+    );
+    let data = await jokes.json();
+    // render function
+    renderJokes(data);
+  } catch (e) {
+    console.log(e + "something went wrong");
+  }
+}
+
+// render in UI
+function renderJokes(data) {
+  try {
+    jokeContainer.textContent = `❝ ${data?.value} ❞`;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+btnNext.addEventListener("click", fetchJokesData);
